bbd6191f657e60790b64152be71e3a6dde56ce15
Mahmut <86982257+mahmutcelik53@users.noreply.github.com>
2021-09-14 14:29:18 +0300

    42_piscine
    c03

---
+++ c03/ex02/ft_strcat.c
@@ -0,0 +1,18 @@
+char	*ft_strcat(char *dest, char *src)
+{
+	char	*charptr;
+	char	*charptrsrc;
+
+	charptr = dest;
+	charptrsrc = src;
+	while (*charptr)
+		charptr++;
+	while (*charptrsrc)
+	{
+		*charptr = *charptrsrc;
+		charptrsrc++;
+		charptr++;
+	}
+	*charptr = 0;
+	return (dest);
+}
