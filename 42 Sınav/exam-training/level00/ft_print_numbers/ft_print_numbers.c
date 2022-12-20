6c5c1cf601b31413fd1ccd2c9cb06f0a87237e56
Mahmut <86982257+mahmutceelik@users.noreply.github.com>
2022-12-20 12:28:35 +0300

    Add files via upload

---
+++ 42 Sınav/exam-training/level00/ft_print_numbers/ft_print_numbers.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	char nb;
+	
+	nb = '0';
+	while (nb <= '9')
+	{
+		write(1, &nb,1);
+		nb++;
+	}
+}
+
+/*
+int	main(void)
+{
+	ft_print_numbers();
+}
+*/
