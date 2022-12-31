8661e88abc4f0d264a38708192a0eba90541b51a
Mahmut <86982257+mahmutceelik@users.noreply.github.com>
2022-12-31 20:11:04 +0300

    Delete c01 directory

--- c01/ex02/ft_swap.c
+++
@@ -1,8 +0,0 @@
-void	ft_swap(int *a, int *b)
-{
-	int	c;
-
-	c = *b;
-	*b = *a;
-	*a = c;
-}
--- c01/ex04/ft_ultimate_div_mod.c
+++
@@ -1,8 +0,0 @@
-void	ft_ultimate_div_mod(int *a, int *b)
-{
-	int	c;
-
-	c = *a / *b;
-	*b = *a % *b;
-	*a = c;
-}
